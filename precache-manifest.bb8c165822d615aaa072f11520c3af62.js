92863e2ecb0675590acf5055b5d961ebd2b80ffc
Łukasz Badocha <krakow@akademia108.pl>
2019-06-30 16:43:42 +0200

    Updates

---
+++ precache-manifest.bb8c165822d615aaa072f11520c3af62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6940d6f83a4d0354d15014f40faf6e4f",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "fe2cfd23fc8e4506909e",
+    "url": "/react-example/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-example/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fe2cfd23fc8e4506909e",
+    "url": "/react-example/static/js/main.c27bfe10.chunk.js"
+  },
+  {
+    "revision": "fffe8ea8f4a6c0c7c1e3",
+    "url": "/react-example/static/js/runtime~main.c7bd7e12.js"
+  }
+]);
